45d71d71e79defe0f60391c507aede0728e4200b
Ahmad Saleh <ahmadtmsaleh@gmail.com>
2018-10-30 13:03:28 +0300

    test
    test

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { FormGroup,FormControl } from '@angular/forms';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { ThanksPage } from '../pages/thanks/thanks';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,ThanksPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    ThanksPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},FormGroup,FormControl
+  ]
+})
+export class AppModule {}
---
+++ src/pages/home/home.ts
@@ -0,0 +1,32 @@
+
+import { Component } from '@angular/core';
+//import {NavController} from 'ionic-angular'
+
+//import { FormGroup,  FormControl} from '@angular/forms';
+//import { ThanksPage } from '../thanks/thanks';
+
+
+@Component({
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  //q1;
+  /*q2;
+  q3;*/
+  //langForm;
+
+  constructor(/*public navCtrl:NavController*/) {
+    /*this.langForm = new FormGroup({
+      "q1": new FormControl({})/*,
+      "q2": new FormControl({}),
+      "q3": new FormControl({})
+    });*/
+  }
+
+  //doSubmit(event) {
+    //console.log('Submitting form', this.langForm.value);
+    //event.preventDefault();
+    //this.navCtrl.push(ThanksPage);
+  //}
+}
